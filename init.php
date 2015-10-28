696784fb3c0ebdcf10bf90bd281ad1ec5d03efc5
Haries Nur Ikhwan <hariesnurikhwan@gmail.com>
2015-10-28 10:29:42 +0800

    Rework the script functions.
    -Add MobileDetect Library
    
    -Add few functions
    —getDateTime
    —getIP
    —getRequestHeader
    —getUniqueID
    —setCookie
    
    -Modify init.php to load all classes and functions
    
    —Add main.php to check if user has visited site

--- init.php
+++ init.php
@@ -1,2 +1,11 @@
 <?php
-	require_once './functions/Mobile_Detect.php';
+date_default_timezone_set('Asia/Kuala_Lumpur');
+include_once 'db.config.php';
+
+$folderName = array('functions', 'classes');
+foreach ($folderName as $value) {
+	foreach (glob("{$value}/*.php") as $filename)
+	{
+	    include_once $filename;
+	}
+}
